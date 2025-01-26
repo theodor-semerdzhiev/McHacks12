9d29f524781d417a01b8eecf6b107887081884b8
Theodor Semerdzhiev <tedi.semerdjiev@hotmail.com>
2025-01-26 10:48:47 -0500

    .

--- frontend/my-next-app/app/ml-approach/page.js
+++ frontend/my-next-app/app/ml-approach/page.js
@@ -1,13 +1,18 @@
-// app/ml-approach/page.js
+
 export default function MlApproachPage() {
     return (
       <>
         <h1>Machine Learning Approach</h1>
         <p>
-          This is a placeholder. In the future, we will implement various 
-          ML-based features or visualizations here.
+        This Jupyter Notebook showcases our real-time data analysis, a Long short-term memory (LSTM) Architecture based RNN model, training processes, and visual insights that powered our solution. Dive into the details and witness how data meets creativity in this interactive, step-by-step breakdown of our Jupyter Notebook!
         </p>
+        <div style={{ width: "100%", height: "80vh", overflow: "hidden", border: "1px solid #ddd" }}>
+          <iframe
+            src={`/jupyter//analysis.html`}
+            title="Machine Learning Notebook"
+            style={{ width: "100%", height: "100%", border: "none" }}
+          ></iframe>
+        </div>
       </>
     );
   }
-  
